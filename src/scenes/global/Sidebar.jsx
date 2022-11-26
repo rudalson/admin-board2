a1429d8766b81d05a0f282d4194348c356e124e3
rudalson <rudalson@gmail.com>
2022-11-26 18:20:02 +0900

    ✨ deployed github io

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -108,10 +108,10 @@ const Sidebar = () => {
                   fontWeight='bold'
                   sx={{ m: '10px 0 0 0' }}
                 >
-                  Ed Roh
+                  Rudalson
                 </Typography>
                 <Typography variant='h5' color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Administrator
                 </Typography>
               </Box>
             </Box>
